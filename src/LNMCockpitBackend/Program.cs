c27615842cd4518f9f9d417704cb4d21882f048d
Stefan Lekrem <lekrem@icloud.com>
2023-10-12 01:41:43 +0200

    Update Program.cs

--- src/LNMCockpitBackend/Program.cs
+++ src/LNMCockpitBackend/Program.cs
@@ -1,15 +1,28 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
+builder.Services
+    .AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(x =>
+    {
+        x.Cookie.Name = ".lnmc.auth";
+        x.Cookie.HttpOnly = true;
+        x.Cookie.MaxAge = TimeSpan.FromDays(1);
+        x.Cookie.SameSite = SameSiteMode.Strict;
+    });
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
 app.UseDefaultFiles()
-    .UseStaticFiles();
-app.UseHttpsRedirection();
-app.UseAuthorization();
+    .UseStaticFiles()
+    .UseAuthentication()
+    .UseAuthorization()
+    .UseHttpsRedirection();
+
 app.MapControllers();
 app.Run();
