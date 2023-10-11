451371174195c5fafa444fbd9ae3b4d89ebc0e57
Stefan Lekrem <lekrem@icloud.com>
2023-10-12 01:50:46 +0200

    Update app-shell.js

--- src/LNMCockpitFrontend/src/js/app-shell.js
+++ src/LNMCockpitFrontend/src/js/app-shell.js
@@ -1,8 +1,31 @@
 import { html, LitElement } from 'lit';
 
+import './views/login-view';
+
 export default class AppShell extends LitElement {
+    static properties = {
+        _view: String
+    };
+
     createRenderRoot = () => this;
-    render = () => { return html`asd`; }
+    render = () => {
+        switch (this._view) {
+            case 'app':
+                return html`app`;
+            case 'login':
+            default:
+                return html`<login-view></login-view>`;
+        }
+    }
+
+    firstUpdated = async () => {
+        const response = await fetch('/api/auth/is-authenticated');
+        const data = await response.json();
+        if (data.isAuthenticated)
+            this._view = 'app';
+        else
+            this._view = 'login';
+    };
 }
 
 customElements.define('app-shell', AppShell);
