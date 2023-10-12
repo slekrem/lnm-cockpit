baf010461155ca9cbb8abc7e36a047da00b95094
Stefan Lekrem <lekrem@icloud.com>
2023-10-12 05:22:11 +0200

    Create OhlcChartModel.cs

---
+++ src/LNMCockpitBackend/Models/Chart/OhlcChartModel.cs
@@ -0,0 +1,20 @@
+﻿namespace LNMCockpit.Models.Chart
+{
+    public class OhlcChartModel
+    {
+        private readonly DateTime _dateTime;
+
+        public OhlcChartModel(DateTime dateTime)
+        {
+            _dateTime = dateTime;
+        }
+
+        public decimal O { get; set; }
+        public decimal H { get; set; }
+        public decimal L { get; set; }
+        public decimal C { get; set; }
+        public int V { get; set; }
+        public long X => _dateTime.ToUnixTimeInMilliseconds();
+        public DateTime DateTime => _dateTime;
+    }
+}
