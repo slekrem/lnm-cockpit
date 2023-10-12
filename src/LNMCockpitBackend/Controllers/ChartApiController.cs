e5228d21a636c5d7eb775fc39fc510213abcef6f
Stefan Lekrem <lekrem@icloud.com>
2023-10-12 03:19:21 +0200

    Create ChartApiController.cs

---
+++ src/LNMCockpitBackend/Controllers/ChartApiController.cs
@@ -0,0 +1,23 @@
+﻿namespace LNMCockpit.Controllers
+{
+    using LNMCockpit.Services;
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+
+    [Authorize]
+    public class ChartApiController : ControllerBase
+    {
+        private readonly ILnMarketsService _lnMarketsService;
+
+        public ChartApiController(ILnMarketsService lnMarketsService)
+        {
+            _lnMarketsService = lnMarketsService;
+        }
+
+        [Route("api/chart")]
+        public IActionResult Index()
+        {
+            return Ok(new { });
+        }
+    }
+}
