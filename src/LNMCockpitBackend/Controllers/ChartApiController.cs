43c755b156ae02273d9b5eee7e1b85e2c69e65c6
Stefan Lekrem <lekrem@icloud.com>
2023-10-12 06:27:36 +0200

    Update ChartApiController.cs

--- src/LNMCockpitBackend/Controllers/ChartApiController.cs
+++ src/LNMCockpitBackend/Controllers/ChartApiController.cs
@@ -29,8 +29,8 @@
                 var grouppedPriceHistoryPriceHistoryData = priceHistoryPriceHistoryData
                     .GroupBy(x => Helper.RoundToNearestNMinutes(x.DateTime, 1))
                     .ToList();
-                var ohlcData = new List<OhlcChartModel>();
-                grouppedPriceHistoryPriceHistoryData.ForEach(x => ohlcData.Add(new OhlcChartModel(x.Key)
+                var ohlcChartData = new List<OhlcChartModel>();
+                grouppedPriceHistoryPriceHistoryData.ForEach(x => ohlcChartData.Add(new OhlcChartModel(x.Key)
                 {
                     O = x.First().Value,
                     H = x.Max(y => y.Value),
@@ -38,9 +38,84 @@
                     C = x.Last().Value
                 }));
 
-                return Ok(new 
+                var openTradesChartData = new List<TradeChartModel>();
+                var openTradesData = await _lnMarketsService.FuturesGetOpenTradesAsync(from, to);
+                openTradesData.ToList().ForEach(x =>
                 {
-                    ohlcData
+                    openTradesChartData.Add(new TradeChartModel
+                    {
+                        X = x.creation_ts - 7200000,
+                        Y = x.price,
+                        Start = true,
+                        BorderColor = x.type == "l" ? "#00ff00" : "ff0000"
+                    });
+                    openTradesChartData.Add(new TradeChartModel
+                    {
+                        X = ohlcChartData.Last().X,
+                        Y = x.price,
+                        Start = false,
+                        BorderColor = x.type == "l" ? "#00ff00" : "ff0000"
+                    });
+
+                    if(x.liquidation > 0)
+                    {
+                        openTradesChartData.Add(new TradeChartModel
+                        {
+                            X = x.creation_ts - 7200000,
+                            Y = x.liquidation,
+                            Start = true,
+                            BorderColor = "#ff0000"
+                        });
+                        openTradesChartData.Add(new TradeChartModel
+                        {
+                            X = ohlcChartData.Last().X,
+                            Y = x.liquidation,
+                            Start = false,
+                            BorderColor = "#ff0000"
+                        });
+                    }
+
+                    if (x.stoploss > 0)
+                    {
+                        openTradesChartData.Add(new TradeChartModel
+                        {
+                            X = x.creation_ts - 7200000,
+                            Y = x.stoploss,
+                            Start = true,
+                            BorderColor = "#ff0000"
+                        });
+                        openTradesChartData.Add(new TradeChartModel
+                        {
+                            X = ohlcChartData.Last().X,
+                            Y = x.stoploss,
+                            Start = false,
+                            BorderColor = "#ff0000"
+                        });
+                    }
+
+                    if (x.takeprofit > 0)
+                    {
+                        openTradesChartData.Add(new TradeChartModel
+                        {
+                            X = x.creation_ts - 7200000,
+                            Y = x.takeprofit,
+                            Start = true,
+                            BorderColor = "#00ff00"
+                        });
+                        openTradesChartData.Add(new TradeChartModel
+                        {
+                            X = ohlcChartData.Last().X,
+                            Y = x.takeprofit,
+                            Start = false,
+                            BorderColor = "#00ff00"
+                        });
+                    }
+                });
+
+                return Ok(new
+                {
+                    ohlcChartData,
+                    openTradesChartData
                 });
 
             }
