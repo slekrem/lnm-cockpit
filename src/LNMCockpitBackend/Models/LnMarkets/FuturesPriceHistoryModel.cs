bb4f326b006ae0d0fbe3e7376d9210e64e4305d7
Stefan Lekrem <lekrem@icloud.com>
2023-10-12 05:22:04 +0200

    Create FuturesPriceHistoryModel.cs

---
+++ src/LNMCockpitBackend/Models/LnMarkets/FuturesPriceHistoryModel.cs
@@ -0,0 +1,11 @@
+﻿namespace LNMCockpit.Models.LnMarkets
+{
+    public class FuturesPriceHistoryModel
+    {
+        public long Time { get; set; }
+
+        public decimal Value { get; set; }
+
+        public DateTime DateTime => DateTimeOffset.FromUnixTimeSeconds(Time / 1000).UtcDateTime;
+    }
+}
