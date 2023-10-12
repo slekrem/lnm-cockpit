7416eaf75f8fdd9ad84255fa6e2d1627a4d8c454
Stefan Lekrem <lekrem@icloud.com>
2023-10-12 05:22:47 +0200

    Update ILnMarketsService.cs

--- src/LNMCockpitBackend/Services/ILnMarketsService.cs
+++ src/LNMCockpitBackend/Services/ILnMarketsService.cs
@@ -1,6 +1,9 @@
 ﻿namespace LNMCockpit.Services
 {
+    using LNMCockpit.Models.LnMarkets;
+
     public interface ILnMarketsService
     {
+        Task<IEnumerable<FuturesPriceHistoryModel>> FuturesGetPriceHistoryAsync(long from, long to, int limit = int.MaxValue);
     }
 }
