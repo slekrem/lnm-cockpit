d82a0536e8d32721037c9a7b56290d81df2d0b3f
Stefan Lekrem <lekrem@icloud.com>
2023-10-12 05:22:42 +0200

    Update LnMarketsService.cs

--- src/LNMCockpitBackend/Services/LnMarketsService.cs
+++ src/LNMCockpitBackend/Services/LnMarketsService.cs
@@ -1,6 +1,58 @@
 ﻿namespace LNMCockpit.Services
 {
+    using LNMCockpit.Models.LnMarkets;
+    using System.Security.Cryptography;
+    using System.Text;
+
     public class LnMarketsService : ILnMarketsService
     {
+        private readonly string _lnMarketsEndpoint = "https://api.lnmarkets.com";
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public LnMarketsService(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public async Task<IEnumerable<FuturesPriceHistoryModel>> FuturesGetPriceHistoryAsync(long from, long to, int limit = int.MaxValue)
+        {
+            var method = "GET";
+            var path = "/v2/futures/history/price";
+            var @params = $"from={from}&to={to}&limit={limit}";
+            var timestamp = GetTimestamp();
+
+            using var client = GetLnMarketsHttpClient(GetSignature($"{timestamp}{method}{path}{@params}"), timestamp);
+            var data = await client.GetFromJsonAsync<IEnumerable<FuturesPriceHistoryModel>>($"{_lnMarketsEndpoint}{path}?{@params}");
+            data = data?.OrderBy(x => x.Time).ToList() ?? new List<FuturesPriceHistoryModel>();
+            return data;
+        }
+
+        private string GetSignature(string payload)
+        {
+            var secret = _httpContextAccessor.HttpContext?.User.Claims
+                .FirstOrDefault(x => x.Type == "secret")?.Value ?? string.Empty;
+            using var hmac = new HMACSHA256(Encoding.UTF8.GetBytes(secret));
+            var hash = hmac.ComputeHash(Encoding.UTF8.GetBytes(payload));
+            return Convert.ToBase64String(hash);
+        }
+
+        private HttpClient GetLnMarketsHttpClient(string signature, long timestamp)
+        {
+            var httpClient = new HttpClient();
+            var key = _httpContextAccessor.HttpContext?.User.Claims
+                .FirstOrDefault(x => x.Type == "key")?.Value ?? string.Empty;
+            var passphrase = _httpContextAccessor.HttpContext?.User.Claims
+                .FirstOrDefault(x => x.Type == "passphrase")?.Value ?? string.Empty;
+
+            httpClient.DefaultRequestHeaders.Add("LNM-ACCESS-KEY", key);
+            httpClient.DefaultRequestHeaders.Add("LNM-ACCESS-PASSPHRASE", passphrase);
+            httpClient.DefaultRequestHeaders.Add("LNM-ACCESS-SIGNATURE", signature);
+            httpClient.DefaultRequestHeaders.Add("LNM-ACCESS-TIMESTAMP", timestamp.ToString());
+            httpClient.DefaultRequestHeaders.TryAddWithoutValidation("Content-Type", "application/json");
+
+            return httpClient;
+        }
+
+        private static long GetTimestamp() => (long)DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc)).TotalMilliseconds;
     }
 }
