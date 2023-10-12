3a99f9231d20fc03b6c6c455e730460d916add56
Stefan Lekrem <lekrem@icloud.com>
2023-10-12 06:26:59 +0200

    Create TradeChartModel.cs

---
+++ src/LNMCockpitBackend/Models/Chart/TradeChartModel.cs
@@ -0,0 +1,13 @@
+﻿namespace LNMCockpit.Models.Chart
+{
+    public class TradeChartModel
+    {
+        public long X { get; set; }
+
+        public decimal Y { get; set; }
+
+        public bool Start { get; set; }
+
+        public string? BorderColor { get; set; }
+    }
+}
