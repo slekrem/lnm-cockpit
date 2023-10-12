ef74ec0d328b240ec35c84391a351202b66efa5b
Stefan Lekrem <lekrem@icloud.com>
2023-10-12 05:22:14 +0200

    Create Helper.cs

---
+++ src/LNMCockpitBackend/Helper.cs
@@ -0,0 +1,18 @@
+﻿namespace LNMCockpit
+{
+    public static class Helper
+    {
+        public static long ToUnixTimeInMilliseconds(this DateTime dateTime)
+            => (long)dateTime.Subtract(new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc)).TotalMilliseconds;
+
+        public static DateTime RoundToNearestNMinutes(DateTime dateTime, int NinMinutes)
+        {
+            var minute = dateTime.Minute;
+            var remainder = minute % NinMinutes;
+            if (remainder != 0)
+                dateTime = dateTime.AddMinutes(NinMinutes - remainder);
+            return new DateTime(dateTime.Year, dateTime.Month, dateTime.Day, dateTime.Hour, dateTime.Minute, 0);
+        }
+
+    }
+}
