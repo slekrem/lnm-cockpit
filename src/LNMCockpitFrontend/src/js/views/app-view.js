cc3b079feb60222d007a4715a4b666cef679bfc0
Stefan Lekrem <lekrem@icloud.com>
2023-10-12 13:01:19 +0200

    Update app-view.js

--- src/LNMCockpitFrontend/src/js/views/app-view.js
+++ src/LNMCockpitFrontend/src/js/views/app-view.js
@@ -14,15 +14,38 @@ export default class AppView extends LitElement {
         _chart: Object,
     };
 
+    _renderNavbar = () => html`
+    <nav class="navbar bg-body-tertiary fixed-top">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="/">LNM Cockpit</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+                <div class="offcanvas-header">
+                    <h5 class="offcanvas-title" id="offcanvasNavbarLabel">LNM Cockpit Menu</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                </div>
+                <div class="offcanvas-body">
+                    <div class="d-flex align-items-end justify-content-end h-100">
+                        <button class="btn btn-danger w-100" @click="${this._onLogOutClick}">Log out</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </nav>
+    `;
+
     createRenderRoot = () => this;
     render = () => {
         return html`
-        <div class="container">
+        ${this._renderNavbar()}
+        <div class="container mt-5">
             <div class="text-center">
                 <h1 class="display-1">My LNM Cockpit</h1>                
-                <div class="card">
-                    <canvas class="card-img-top"></canvas>
-                </div>
+            </div>
+            <div class="card mt-5">
+                <canvas class="card-img-top"></canvas>
             </div>
         </div>
         `;
@@ -106,6 +129,12 @@ export default class AppView extends LitElement {
                 }]
             }
         });
+    };
+
+    _onLogOutClick = async (e) => {
+        e.preventDefault();
+        await fetch('/api/auth/logout', { method: 'POST' });
+        location.reload();
     };
 }
 
