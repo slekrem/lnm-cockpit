e9fdfc4f77c35f0cb82d7bbd840718a1e3e42134
Stefan Lekrem <lekrem@icloud.com>
2023-10-12 05:23:00 +0200

    Update ChartApiController.cs

--- src/LNMCockpitBackend/Controllers/ChartApiController.cs
+++ src/LNMCockpitBackend/Controllers/ChartApiController.cs
@@ -1,8 +1,11 @@
 ﻿namespace LNMCockpit.Controllers
 {
     using LNMCockpit.Services;
+    using Microsoft.AspNetCore.Authentication.Cookies;
+    using Microsoft.AspNetCore.Authentication;
     using Microsoft.AspNetCore.Authorization;
     using Microsoft.AspNetCore.Mvc;
+    using LNMCockpit.Models.Chart;
 
     [Authorize]
     public class ChartApiController : ControllerBase
@@ -14,10 +17,38 @@
             _lnMarketsService = lnMarketsService;
         }
 
-        [Route("api/chart")]
-        public IActionResult Index()
+        [Route("api/chart/data")]
+        public async Task<IActionResult> ChartData()
         {
-            return Ok(new { });
+            var from = DateTime.UtcNow.AddDays(-1).ToUnixTimeInMilliseconds();
+            var to = DateTime.UtcNow.ToUnixTimeInMilliseconds();
+            try
+            {
+                var priceHistoryPriceHistoryData = await _lnMarketsService
+                    .FuturesGetPriceHistoryAsync(from, to);
+                var grouppedPriceHistoryPriceHistoryData = priceHistoryPriceHistoryData
+                    .GroupBy(x => Helper.RoundToNearestNMinutes(x.DateTime, 1))
+                    .ToList();
+                var ohlcData = new List<OhlcChartModel>();
+                grouppedPriceHistoryPriceHistoryData.ForEach(x => ohlcData.Add(new OhlcChartModel(x.Key)
+                {
+                    O = x.First().Value,
+                    H = x.Max(y => y.Value),
+                    L = x.Min(y => y.Value),
+                    C = x.Last().Value
+                }));
+
+                return Ok(new 
+                {
+                    ohlcData
+                });
+
+            }
+            catch (Exception ex)
+            {
+                await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+                return BadRequest(new { ex.Message });
+            }
         }
     }
 }
