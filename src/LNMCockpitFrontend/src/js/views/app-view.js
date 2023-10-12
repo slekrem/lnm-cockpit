f2e0704afb369ae6f4e86d002b7c259e4b325349
Stefan Lekrem <lekrem@icloud.com>
2023-10-12 05:22:26 +0200

    Update app-view.js

--- src/LNMCockpitFrontend/src/js/views/app-view.js
+++ src/LNMCockpitFrontend/src/js/views/app-view.js
@@ -1,14 +1,114 @@
 import { html, LitElement } from 'lit';
+import { Chart, registerables } from 'chart.js';
+import {
+    OhlcElement,
+    OhlcController,
+    CandlestickElement,
+    CandlestickController
+} from 'chartjs-chart-financial';
+import 'chartjs-adapter-luxon';
+import zoomPlugin from 'chartjs-plugin-zoom';
 
 export default class AppView extends LitElement {
-    static properties = { };
+    static properties = { 
+        _chart: Object,
+    };
 
     createRenderRoot = () => this;
     render = () => {
-        return html``;
+        return html`
+        <div class="container">
+            <div class="text-center">
+                <h1 class="display-1">My LNM Cockpit</h1>                
+                <div class="card">
+                    <canvas class="card-img-top"></canvas>
+                </div>
+            </div>
+        </div>
+        `;
     }
 
-    firstUpdated = async () => { };
+    firstUpdated = async () => {
+        const response = await fetch('/api/chart/data');
+        if(!response.ok){
+            location.reload();
+            return;
+        }
+        const responseData = await response.json();
+
+        const context = this.querySelector('canvas').getContext('2d');
+        const options = {
+            plugins: {
+                zoom: {
+                    zoom: {
+                        wheel: {
+                            enabled: true,
+                        },
+                        pinch: {
+                            enabled: false
+                        },
+                        mode: 'xy',
+                    },
+                    pan: {
+                        enabled: true
+                    },
+                }
+            }
+        };
+        const data = {
+            datasets: [{
+                label: 'Bitcoin / U.S. Dollar',
+                data: responseData.ohlcData
+            }, {
+                type: 'line',
+                label: 'Open',
+                data: [],
+                //borderColor: 'rgb(255,0,0)',
+                segment: {
+                    borderColor: ctx => {
+                        if (ctx.p0.raw.isStart)
+                            return ctx.p0.raw.borderColor;
+                        return 'rgba(75,192,192,0)';
+                    }
+                }
+            }, {
+                type: 'line',
+                label: 'Running',
+                data: [],
+                //borderColor: 'rgb(255,0,0)',
+                segment: {
+                    borderColor: ctx => {
+                        if (ctx.p0.raw.isStart)
+                            return ctx.p0.raw.borderColor;
+                        return 'rgba(75,192,192,0)';
+                    }
+                }
+            }, {
+                type: 'line',
+                label: 'Closed',
+                data: [],
+                //borderColor: 'rgb(255,0,0)',
+                segment: {
+                    borderColor: ctx => {
+                        if (ctx.p0.raw.isStart)
+                            return ctx.p0.raw.borderColor;
+                        return 'rgba(75,192,192,0)';
+                    }
+                }
+            }]
+        };
+        const config = { type: 'ohlc', options, data };
+
+        Chart.register(
+            ...registerables,
+            zoomPlugin,
+            OhlcElement,
+            OhlcController,
+            CandlestickElement,
+            CandlestickController);
+
+        this._chart = new Chart(context, config);
+    };
 }
 
 customElements.define('app-view', AppView);
