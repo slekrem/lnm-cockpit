e50d89e0534681f24b41a7a336c287d430021243
Stefan Lekrem <lekrem@icloud.com>
2023-10-31 12:40:55 +0100

    Update trades-tables.js

--- src/LNMCockpitFrontend/src/js/components/trades-tables.js
+++ src/LNMCockpitFrontend/src/js/components/trades-tables.js
@@ -43,6 +43,207 @@ export default class TradesTables extends LitElement {
         e.target.classList.add('active')
         this._table = e.target.dataset.table;
     };
+
+    /*
+    _onTableClick = (e) => {
+        e.preventDefault();
+        [...this.querySelectorAll('a.nav-link.trades-table.active')].map(x => x.classList.remove('active'));
+        e.target.classList.add('active')
+        const table = e.target.dataset.table;
+        this._table = table;
+    };
+
+    _onHideClick = (e) => {
+        e.preventDefault();
+        let tradeId = e.target.dataset.tradeId;
+        let table = e.target.dataset.table;
+        let name = e.target.dataset.name;
+        if (!tradeId) {
+            tradeId = e.target.parentNode.dataset.tradeId;
+            table = e.target.parentNode.dataset.table;
+            name = e.target.parentNode.dataset.name;
+        }
+
+        switch (table) {
+            case 'open':
+                this._hideOpenTradeData(tradeId, name);
+                break;
+            case 'running':
+                this._hideRunningTradeData(tradeId, name);
+                break;
+            case 'closed':
+                this._hideClosedTradeData(tradeId, name);
+                break;
+            default:
+                break;
+        }
+        this.requestUpdate();
+        this._ohlcChart.update();
+    };
+
+    _hideOpenTradeData = (tradeId, name) => {
+        switch (name) {
+            case 'quantity':
+                const quantityHide = this._data.openTradesData.find(x => x.id === tradeId).quantityHide;
+                this._data.openTradesData.find(x => x.id === tradeId).quantityHide = !quantityHide;
+                break;
+            case 'side':
+                const sideHide = this._data.openTradesData.find(x => x.id === tradeId).sideHide;
+                this._data.openTradesData.find(x => x.id === tradeId).sideHide = !sideHide;
+                break;
+            case 'price':
+                const priceHide = this._data.openTradesData.find(x => x.id === tradeId).priceHide;
+                this._data.openTradesData.find(x => x.id === tradeId).priceHide = !priceHide;
+                this._ohlcChart.config.data.datasets
+                    .find(x => x.label === 'Open').data
+                    .find(x => x.id === tradeId && x.type === 'price').hide = !priceHide;
+                break;
+            case 'liquidation':
+                const liquidationHide = this._data.openTradesData.find(x => x.id === tradeId).liquidationHide;
+                this._data.openTradesData.find(x => x.id === tradeId).liquidationHide = !liquidationHide;
+                this._ohlcChart.config.data.datasets
+                    .find(x => x.label === 'Open').data
+                    .find(x => x.id === tradeId && x.type === 'liquidation').hide = !liquidationHide;
+                break;
+            case 'leverage':
+                const leverageHide = this._data.openTradesData.find(x => x.id === tradeId).leverageHide;
+                this._data.openTradesData.find(x => x.id === tradeId).leverageHide = !leverageHide;
+                this._ohlcChart.config.data.datasets
+                    .find(x => x.label === 'Open').data
+                    .find(x => x.id === tradeId && x.type === 'liquidation').hide = !leverageHide;
+                break;
+            case 'margin':
+                const marginHide = this._data.openTradesData.find(x => x.id === tradeId).marginHide;
+                this._data.openTradesData.find(x => x.id === tradeId).marginHide = !marginHide;
+                break;
+            case 'stoploss':
+                const stoplossHide = this._data.openTradesData.find(x => x.id === tradeId).stoplossHide;
+                this._data.openTradesData.find(x => x.id === tradeId).stoplossHide = !stoplossHide;
+                try {
+                    this._ohlcChart.config.data.datasets
+                        .find(x => x.label === 'Open').data
+                        .find(x => x.id === tradeId && x.type === 'stoploss').hide = !stoplossHide;
+                } catch { }
+                break;
+            case 'takeprofit':
+                const takeprofitHide = this._data.openTradesData.find(x => x.id === tradeId).takeprofitHide;
+                this._data.openTradesData.find(x => x.id === tradeId).takeprofitHide = !takeprofitHide;
+                try {
+                    this._ohlcChart.config.data.datasets
+                        .find(x => x.label === 'Open').data
+                        .find(x => x.id === tradeId && x.type === 'takeprofit').hide = !takeprofitHide;
+                } catch { }
+                break;
+            default:
+                break;
+        }
+    };
+
+    _hideRunningTradeData = (tradeId, name) => {
+        switch (name) {
+            case 'quantity':
+                const quantityHide = this._data.runningTradesData.find(x => x.id === tradeId).quantityHide;
+                this._data.runningTradesData.find(x => x.id === tradeId).quantityHide = !quantityHide;
+                break;
+            case 'side':
+                const sideHide = this._data.runningTradesData.find(x => x.id === tradeId).sideHide;
+                this._data.runningTradesData.find(x => x.id === tradeId).sideHide = !sideHide;
+                break;
+            case 'price':
+                const priceHide = this._data.runningTradesData.find(x => x.id === tradeId).priceHide;
+                this._data.runningTradesData.find(x => x.id === tradeId).priceHide = !priceHide;
+                this._ohlcChart.config.data.datasets
+                    .find(x => x.label === 'Running').data
+                    .find(x => x.id === tradeId && x.type === 'price').hide = !priceHide;
+                break;
+            case 'liquidation':
+                const liquidationHide = this._data.runningTradesData.find(x => x.id === tradeId).liquidationHide;
+                this._data.runningTradesData.find(x => x.id === tradeId).liquidationHide = !liquidationHide;
+                this._ohlcChart.config.data.datasets
+                    .find(x => x.label === 'Running').data
+                    .find(x => x.id === tradeId && x.type === 'liquidation').hide = !liquidationHide;
+                break;
+            case 'leverage':
+                const leverageHide = this._data.runningTradesData.find(x => x.id === tradeId).leverageHide;
+                this._data.runningTradesData.find(x => x.id === tradeId).leverageHide = !leverageHide;
+                break;
+            case 'margin':
+                const marginHide = this._data.runningTradesData.find(x => x.id === tradeId).marginHide;
+                this._data.runningTradesData.find(x => x.id === tradeId).marginHide = !marginHide;
+                break;
+            case 'stoploss':
+                const stoplossHide = this._data.runningTradesData.find(x => x.id === tradeId).stoplossHide;
+                this._data.runningTradesData.find(x => x.id === tradeId).stoplossHide = !stoplossHide;
+                try {
+                    this._ohlcChart.config.data.datasets
+                        .find(x => x.label === 'Running').data
+                        .find(x => x.id === tradeId && x.type === 'stoploss').hide = !stoplossHide;
+                } catch { }
+                break;
+            case 'takeprofit':
+                const takeprofitHide = this._data.runningTradesData.find(x => x.id === tradeId).takeprofitHide;
+                this._data.runningTradesData.find(x => x.id === tradeId).takeprofitHide = !takeprofitHide;
+                try {
+                    this._ohlcChart.config.data.datasets
+                        .find(x => x.label === 'Running').data
+                        .find(x => x.id === tradeId && x.type === 'takeprofit').hide = !takeprofitHide;
+                } catch { }
+                break;
+            case 'pl':
+                const plHide = this._data.runningTradesData.find(x => x.id === tradeId).plHide;
+                this._data.runningTradesData.find(x => x.id === tradeId).plHide = !plHide;
+                break;
+            default:
+                break;
+        }
+    };
+
+    _hideClosedTradeData = (tradeId, name) => {
+        switch (name) {
+            case 'quantity':
+                const quantityHide = this._data.closedTradesData.find(x => x.id === tradeId).quantityHide;
+                this._data.closedTradesData.find(x => x.id === tradeId).quantityHide = !quantityHide;
+                break;
+            case 'side':
+                const sideHide = this._data.closedTradesData.find(x => x.id === tradeId).sideHide;
+                this._data.closedTradesData.find(x => x.id === tradeId).sideHide = !sideHide;
+                break;
+            case 'price':
+                const priceHide = this._data.closedTradesData.find(x => x.id === tradeId).priceHide;
+                this._data.closedTradesData.find(x => x.id === tradeId).priceHide = !priceHide;
+                this._ohlcChart.config.data.datasets
+                    .find(x => x.label === 'Closed').data
+                    .find(x => x.id === tradeId && x.type === 'price').hide = !priceHide;
+                break;
+            case 'liquidation':
+                const liquidationHide = this._data.closedTradesData.find(x => x.id === tradeId).liquidationHide;
+                this._data.closedTradesData.find(x => x.id === tradeId).liquidationHide = !liquidationHide;
+                break;
+            case 'leverage':
+                const leverageHide = this._data.closedTradesData.find(x => x.id === tradeId).leverageHide;
+                this._data.closedTradesData.find(x => x.id === tradeId).leverageHide = !leverageHide;
+                break;
+            case 'margin':
+                const marginHide = this._data.closedTradesData.find(x => x.id === tradeId).marginHide;
+                this._data.closedTradesData.find(x => x.id === tradeId).marginHide = !marginHide;
+                break;
+            case 'stoploss':
+                const stoplossHide = this._data.closedTradesData.find(x => x.id === tradeId).stoplossHide;
+                this._data.closedTradesData.find(x => x.id === tradeId).stoplossHide = !stoplossHide;
+                break;
+            case 'takeprofit':
+                const takeprofitHide = this._data.closedTradesData.find(x => x.id === tradeId).takeprofitHide;
+                this._data.closedTradesData.find(x => x.id === tradeId).takeprofitHide = !takeprofitHide;
+                break;
+            case 'pl':
+                const plHide = this._data.closedTradesData.find(x => x.id === tradeId).plHide;
+                this._data.closedTradesData.find(x => x.id === tradeId).plHide = !plHide;
+                break;
+            default:
+                break;
+        }
+    };
+    */
 };
 
 customElements.define('trades-tables', TradesTables);
