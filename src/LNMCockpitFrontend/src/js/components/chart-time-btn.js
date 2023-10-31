776051b287b1a56bf2ee54094fd1a5643dd6bc26
Stefan Lekrem <lekrem@icloud.com>
2023-10-31 12:40:30 +0100

    Update chart-time-btn.js

--- src/LNMCockpitFrontend/src/js/components/chart-time-btn.js
+++ src/LNMCockpitFrontend/src/js/components/chart-time-btn.js
@@ -2,12 +2,14 @@ import { LitElement, html } from "lit";
 
 export default class ChartTimeBtn extends LitElement {
     static properties = {
-        disabled: Boolean
+        disabled: Boolean,
+        _activeView: String
     };
 
     createRenderRoot = () => this;
     render = () => {
         const disabledClass = this.disabled ? 'disabled' : '';
+        const activeClass = (view) => this._activeView === view ? 'active' : '';
         return html`
         <div class="dropdown">
             <button class="btn btn-link link-underline link-underline-opacity-0 dropdown-toggle"
@@ -18,28 +20,30 @@ export default class ChartTimeBtn extends LitElement {
             </button>
             <ul class="dropdown-menu">
                 <li>
-                    <a class="dropdown-item chart-view ${disabledClass}" href="#" data-view="24h1m"
+                    <a class="dropdown-item chart-view ${activeClass('24h1m')} ${disabledClass}"
+                        href="#"
+                        data-view="24h1m"
                         @click="${this._onChartViewClick}">24h in 1min chart</a>
                 </li>
                 <li>
-                    <a class="dropdown-item chart-view ${disabledClass}" href="#" data-view="48h5m" @click="${this._onChartViewClick}">48h in 5min chart</a>
+                    <a class="dropdown-item chart-view ${activeClass('48h5m')} ${disabledClass}"
+                        href="#"
+                        data-view="48h5m"
+                        @click="${this._onChartViewClick}">48h in 5min chart</a>
                 </li>
                 <li>
-                    <a class="dropdown-item chart-view ${disabledClass}"
-                        aria-current="page"
+                    <a class="dropdown-item chart-view ${activeClass('7d1h')} ${disabledClass}"
                         href="#"
                         data-view="7d1h"
                         @click="${this._onChartViewClick}">7d in 1h chart</a>
                 </li>
                 <li>
-                    <a class="dropdown-item chart-view ${disabledClass}"
-                        aria-current="page"
+                    <a class="dropdown-item chart-view ${activeClass('2w4h')} ${disabledClass}"
                         href="#"
                         data-view="2w4h" @click="${this._onChartViewClick}">2w in 4h chart</a>
                 </li>
                 <li>
-                    <a class="dropdown-item chart-view active ${disabledClass}"
-                        aria-current="page"
+                    <a class="dropdown-item chart-view ${activeClass('1m8h')} ${disabledClass}"
                         href="#"
                         data-view="1m8h"
                         @click="${this._onChartViewClick}">1M in 8h chart</a>
@@ -49,12 +53,13 @@ export default class ChartTimeBtn extends LitElement {
         `;
     };
 
+    firstUpdated = () => this._activeView = '1m8h';
+
     _onChartViewClick = async (e) => {
         e.preventDefault();
-        [...this.querySelectorAll('a.dropdown-item.chart-view.active')].map(x => x.classList.remove('active'));
-        e.target.classList.add('active');
+        this._activeView = e.target.dataset.view;
         this.dispatchEvent(new CustomEvent('time-click', {
-            detail: e.target.dataset.view
+            detail: this._activeView
         }));
     };
 }
