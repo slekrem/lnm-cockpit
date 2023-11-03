2c2e9bc74a06d2fb3d1ece510baf5fee8d262e09
Stefan Lekrem <lekrem@icloud.com>
2023-11-03 10:38:35 +0100

    Create CopilotApiController.cs

---
+++ src/LNMCockpitBackend/Controllers/CopilotApiController.cs
@@ -0,0 +1,25 @@
+﻿namespace LNMCockpit.Controllers
+{
+    using LNMCockpit.Services;
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+
+    [Authorize]
+    public class CopilotApiController : ControllerBase
+    {
+        private readonly ILnMarketsService _lnMarketsService;
+
+        public CopilotApiController(ILnMarketsService lnMarketsService)
+        {
+            _lnMarketsService = lnMarketsService;
+        }
+
+        [HttpPost]
+        [Route("api/copilot/create-market-order")]
+        public async Task<IActionResult> CreateMarketOrder(string side)
+        {
+            var result = await _lnMarketsService.FuturesNewTrade(side, "m", 100);
+            return Ok(new { result });
+        }
+    }
+}
