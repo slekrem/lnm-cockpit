d9c1dc738c811fafe976a25b858604c77b8933df
Stefan Lekrem <lekrem@icloud.com>
2023-12-04 16:09:18 +0100

    Update app-shell.js

--- src/LNMCockpitFrontend/src/js/app-shell.js
+++ src/LNMCockpitFrontend/src/js/app-shell.js
@@ -1,18 +1,32 @@
 import { html, LitElement } from 'lit';
 
 import './views/app-view';
+import './views/my-copilot-view';
 import './components/lnmc-navbar';
 
 export default class AppShell extends LitElement {
     static properties = {
-        _isAuthenticated: Boolean
+        _isAuthenticated: Boolean,
+        _view: String,
     };
+
+    _renderView = () => {
+        switch (this._view) {
+            case 'my-copilot':
+                return html`<my-copilot-view></my-copilot-view>`;
+            case 'app':
+                return html`<app-view .isAuthenticated="${this._isAuthenticated}"></app-view>`;
+            default:
+                return html``;
+        };
+    }
 
     createRenderRoot = () => this;
     render = () => {
         return html`
-        <lnmc-navbar .isAuthenticated="${this._isAuthenticated}"></lnmc-navbar>
-        <app-view .isAuthenticated="${this._isAuthenticated}"></app-view>`;
+        <lnmc-navbar .isAuthenticated="${this._isAuthenticated}" @my-copilot-click="${this._onMyCopilotClick}"></lnmc-navbar>
+        ${this._renderView()}
+        `;
     };
 
     firstUpdated = async () => {
@@ -20,6 +34,12 @@ export default class AppShell extends LitElement {
         const response = await fetch('/api/auth/is-authenticated');
         const data = await response.json();
         this._isAuthenticated = data.isAuthenticated;
+        this._view = 'my-copilot';
+    };
+
+    _onMyCopilotClick = e => {
+        e.preventDefault();
+        this._view = 'my-copilot';
     };
 }
 
