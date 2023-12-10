8f6f537fc2adac1dd0a56836c7df9dd7d8699f38
Stefan Lekrem <lekrem@icloud.com>
2023-12-10 17:24:39 +0100

    Update my-copilot-view.js

--- src/LNMCockpitFrontend/src/js/views/my-copilot-view.js
+++ src/LNMCockpitFrontend/src/js/views/my-copilot-view.js
@@ -1,5 +1,14 @@
 import { LitElement, html } from 'lit';
 import { Chart, registerables } from 'chart.js';
+import { LNMCUtilities } from '../LNMCUtilities.js';
+import {
+    OhlcElement,
+    OhlcController,
+    CandlestickElement,
+    CandlestickController
+} from 'chartjs-chart-financial';
+import 'chartjs-adapter-luxon';
+import zoomPlugin from 'chartjs-plugin-zoom';
 
 export default class MyCopilotView extends LitElement {
     static properties = {
@@ -13,13 +22,326 @@ export default class MyCopilotView extends LitElement {
                 <div class="col">
                     <div class="card mt-5">
                         <div class="card-body">
-                            <canvas></canvas>
+                            <div class="row">
+                                <div class="col-12">
+                                    <canvas class="c-1"></canvas>
+                                </div>
+                                <div class="col-12">
+                                    <canvas class="c-0"></canvas>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
         `;
+    };
+
+    firstUpdated = async () => {
+        const window = 60 * 24;
+        const timer = ms => new Promise(res => setTimeout(res, ms));
+        const scaleDown = (ohlc, min, max) => {
+            return {
+                ...ohlc,
+                o: LNMCUtilities.FeatureScaling.scaleDown(ohlc.o, min, max),
+                h: LNMCUtilities.FeatureScaling.scaleDown(ohlc.h, min, max),
+                l: LNMCUtilities.FeatureScaling.scaleDown(ohlc.l, min, max),
+                c: LNMCUtilities.FeatureScaling.scaleDown(ohlc.c, min, max),
+                maxRange: LNMCUtilities.FeatureScaling.scaleDown(ohlc.maxRange, min, max),
+                minRange: LNMCUtilities.FeatureScaling.scaleDown(ohlc.minRange, min, max),
+            };
+        };
+        const scaleUp = (ohlc, min, max) => {
+            return {
+                o: LNMCUtilities.FeatureScaling.scaleUp(ohlc.o, min, max),
+                h: LNMCUtilities.FeatureScaling.scaleDown(ohlc.h, min, max),
+                l: LNMCUtilities.FeatureScaling.scaleDown(ohlc.l, min, max),
+                c: LNMCUtilities.FeatureScaling.scaleDown(ohlc.c, min, max),
+                maxRange: LNMCUtilities.FeatureScaling.scaleDown(ohlc.maxRange, min, max),
+                minRange: LNMCUtilities.FeatureScaling.scaleDown(ohlc.minRange, min, max),
+            };
+        };
+
+        Chart.register(
+            ...registerables,
+            zoomPlugin,
+            OhlcElement,
+            OhlcController,
+            CandlestickElement,
+            CandlestickController);
+        const ctx = this.querySelector('canvas.c-0');
+        this._chart = new Chart(ctx, {
+            type: 'line',
+            options: {
+                plugins: {
+                    title: { display: false },
+                    subtitle: { display: false },
+                    legend: { display: false },
+                },
+            },
+            data: {
+                datasets: [{
+                    data: [],
+                    borderWidth: 0,
+                    backgroundColor: (x) => {
+                        if (x.parsed?.y > 0) return 'green';
+                        return 'red';
+                    },
+                }, {
+                    data: [],
+                    borderWidth: 0,
+                    backgroundColor: 'blue',
+                }, {
+                    data: [],
+                    borderWidth: 0,
+                    backgroundColor: 'blue',
+                }, {
+                    data: [],
+                    borderWidth: 0,
+                    backgroundColor: 'blue',
+                }, {
+                    data: [],
+                    borderWidth: 0,
+                    backgroundColor: 'blue',
+                }, {
+                    data: [
+                        { x: 0, y: 1.0 },
+                        { x: window - 1, y: 1.0 },
+                        { x: 0, y: 0 },
+                        { x: window - 1, y: 0 },
+                        { x: 0, y: -1.0 },
+                        { x: window - 1, y: -1.0 }],
+                    borderWidth: 0,
+                    backgroundColor: 'red',
+                }]
+            },
+        });
+        this._chart2 = new Chart(this.querySelector('canvas.c-1').getContext('2d'), {
+            type: 'ohlc',
+            options: {
+                plugins: {
+                    title: { display: false },
+                    subtitle: { display: false },
+                    legend: { display: false },
+                },
+            },
+            data: {
+                datasets: [{
+                    data: [],
+                    hidden: false
+                }, {
+                    type: 'line',
+                    data: [],
+                    borderWidth: 0,
+                    backgroundColor: 'blue',
+                }, {
+                    type: 'line',
+                    data: [],
+                    borderWidth: 0,
+                    backgroundColor: 'blue',
+                },]
+            },
+        });
+
+        let response = await fetch('/api/copilot/test');
+        let data = await response.json();
+        let processedData = [];
+        for (let i = 0; i < data.length; ++i) {
+            processedData.push({
+                ...data[i],
+                ma: LNMCUtilities.Indicators.ma(processedData, 200)
+            });
+            const range = processedData.slice(window * -1);
+            if (range.length === window) {
+                processedData.slice(-1)[0].maxRange = Math.max(...range.map(x => x.c));
+                processedData.slice(-1)[0].minRange = Math.min(...range.map(x => x.c));
+                let scaledRange = range.map(x => scaleDown(x, processedData.slice(-1)[0].minRange, processedData.slice(-1)[0].maxRange));
+
+
+
+
+                let s = scaledRange.filter(x => x.c < 0.25);
+                let b = scaledRange.filter(x => x.c > 0.75);
+                //console.log(`s: ${s.length} b: ${b.length}`);
+
+
+
+                let last = scaledRange.slice(-1)[0];
+                const target = 0.0;
+                const maxRange = 0.2;
+                const minRange = 0.1;
+                scaledRange = scaledRange.map(x => {
+                    const maxTempRange = x.maxRange ? x.maxRange : maxRange;
+                    const minTempRange = x.minRange ? x.minRange : minRange;
+                    return {
+                        ...x,
+                        maxRange: maxTempRange + (target - last.maxRange ? last.maxRange : maxRange),
+                        minRange: minTempRange + (target - last.minRange ? last.minRange : minRange),
+                    };
+                });
+
+                // console.log(scaledRange.slice(-1)[1]);
+
+
+
+
+
+
+
+
+
+
+                /*this._chart.config.data.labels = scaledRange.map((x, i) => i);
+                this._chart.config.data.datasets[0].data = scaledRange.map((x, i) => { return { x: i, y: x.c }; });
+                this._chart.config.data.datasets[1].data = scaledRange.map((x, i) => { return { x: i, y: x.maxRange }; });
+                this._chart.config.data.datasets[4].data = scaledRange.map((x, i) => { return { x: i, y: x.minRange }; });
+                this._chart.update();
+
+                this._chart2.config.data.datasets[0].data = range;
+                this._chart2.config.data.datasets[1].data = range.map(x => { return { x: x.x, y: x.maxRange } });
+                this._chart2.config.data.datasets[2].data = range.map(x => { return { x: x.x, y: x.minRange } });
+                this._chart2.update();*/
+
+                processedData[processedData.length - 1].maxRange = LNMCUtilities.FeatureScaling
+                    .scaleUp(scaledRange.slice(-1)[0].maxRange,
+                        processedData.slice(-1)[0].minRange,
+                        processedData.slice(-1)[0].maxRange);
+                processedData[processedData.length - 1].maxRangeConst = LNMCUtilities.FeatureScaling
+                    .scaleUp(maxRange, processedData.slice(-1)[0].minRange, processedData.slice(-1)[0].maxRange);
+
+                processedData[processedData.length - 1].minRange = LNMCUtilities.FeatureScaling
+                    .scaleUp(scaledRange.slice(-1)[0].minRange,
+                        processedData.slice(-1)[0].minRange,
+                        processedData.slice(-1)[0].maxRange);
+                processedData[processedData.length - 1].minRangeConst = LNMCUtilities.FeatureScaling
+                    .scaleUp(minRange, processedData.slice(-1)[0].minRange, processedData.slice(-1)[0].maxRange);
+
+                if (true) {
+                    this._chart2.config.data.datasets[0].data = processedData.slice(window * -1);
+                    //this._chart2.config.data.datasets[1].data = processedData.slice(window * -1).map(x => { return { x: x.x, y: x.maxRange } });
+                    //this._chart2.config.data.datasets[2].data = processedData.slice(window * -1).map(x => { return { x: x.x, y: x.minRange } });
+
+                    //LNMCUtilities.Indicators.maTest(this._chart2, window, processedData.slice(window * -1));
+
+                    // if (!this._chart2.config.data.datasets.filter(x => x.id === 'ma').length) {
+                    //     this._chart2.config.data.datasets.push({
+                    //         id: 'ma',
+                    //         type: 'line',
+                    //         data: [],
+                    //         borderWidth: 0,
+                    //         backgroundColor: 'green',
+                    //     });
+                    // }
+                    // else {
+                    //     this._chart2.config.data.datasets.filter(x => x.id === 'ma')[0].data = processedData
+                    //         .slice(window * -1)
+                    //         .map(x => {
+                    //             return {
+                    //                 x: x.x,
+                    //                 y: x.ma
+                    //             }
+                    //         });
+                    // }
+
+                    const rangeMax = [...range].sort((a, b) => a.h - b.h);
+                    const rangeMin = [...range].sort((a, b) => a.l - b.l);
+                    const asd = [, rangeMax.slice(-1)[0], rangeMin[0]].sort((a, b) => a.x - b.x);
+                    if (!this._chart2.config.data.datasets.filter(x => x.id === 'range-ohlc').length) {
+                        this._chart2.config.data.datasets.push({
+                            id: 'range-ohlc',
+                            type: 'line',
+                            data: [{
+                                x: range[0].x,
+                                y: range[0].c
+                            }, {
+                                x: asd[0].x,
+                                y: asd[0].c
+                            }, {
+                                x: range.slice(-1)[0].x,
+                                y: range.slice(-1)[0].c
+                            }],
+                            borderWidth: 5,
+                            borderColor: 'green',
+                            backgroundColor: 'green',
+                        });
+                    }
+                    else {
+                        const steps = 6;
+                        const length = window / steps;
+                        let start = 0;
+                        const geteilteTeile = [];
+                        this._chart2.config.data.datasets.filter(x => x.id === 'range-ohlc')[0].data = [];
+                        for (let i = 0; i < steps; ++i) {
+                            const teil = range.slice(start, start + length);
+                            geteilteTeile.push(teil);
+                            start += length;
+
+                            const open = teil[0];
+                            const close = teil.slice(-1)[0];
+                            const high = teil.sort((a, b) => a.h - b.h);
+                            const low = teil.sort((a, b) => a.l - b.l);
+                            
+                            // console.log(high.slice(-1)[0].h);
+                            // console.log(low[0].l);
+
+                            // this._chart2.config.data.datasets.filter(x => x.id === 'range-ohlc')[0].data.push({
+                            //     x: open.x,
+                            //     y: open.c,
+                            // });
+
+                            if (high.slice(-1)[0].x < low[0].x) {
+                                this._chart2.config.data.datasets.filter(x => x.id === 'range-ohlc')[0].data.push({
+                                    x: high.slice(-1)[0].x,
+                                    y: high.slice(-1)[0].h,
+                                });
+                                this._chart2.config.data.datasets.filter(x => x.id === 'range-ohlc')[0].data.push({
+                                    x: low[0].x,
+                                    y: low[0].l,
+                                });
+                            } else {
+                                this._chart2.config.data.datasets.filter(x => x.id === 'range-ohlc')[0].data.push({
+                                    x: low[0].x,
+                                    y: low[0].l,
+                                });
+                                this._chart2.config.data.datasets.filter(x => x.id === 'range-ohlc')[0].data.push({
+                                    x: high.slice(-1)[0].x,
+                                    y: high.slice(-1)[0].h,
+                                });
+                            }
+                            this._chart2.config.data.datasets.filter(x => x.id === 'range-ohlc')[0].data.push({
+                                x: close.x,
+                                y: close.c,
+                            });
+                        }
+
+                        // this._chart2.config.data.datasets.filter(x => x.id === 'range-ohlc')[0].data = [{
+                        //     x: range[0].x,
+                        //     y: range[0].c
+                        // }, {
+                        //     x: asd[0].x,
+                        //     y: asd[0].c
+                        // }, {
+                        //     x: asd[1].x,
+                        //     y: asd[1].c
+                        // }, {
+                        //     x: range.slice(-1)[0].x,
+                        //     y: range.slice(-1)[0].c
+                        // }]
+                    }
+
+
+
+                    this._chart2.update();
+                    await timer(60);
+                }
+            }
+        }
+
+        this._chart2.config.data.datasets[0].data = processedData.slice(window * -1);
+        //this._chart2.config.data.datasets[1].data = processedData.slice(window * -1).map(x => { return { x: x.x, y: x.maxRangeConst } });
+        //this._chart2.config.data.datasets[2].data = processedData.slice(window * -1).map(x => { return { x: x.x, y: x.minRangeConst } });
+        this._chart2.update();
     };
 
     _2_firstUpdated = async () => {
