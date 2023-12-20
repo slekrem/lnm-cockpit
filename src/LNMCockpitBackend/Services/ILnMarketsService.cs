e67e4dd47bb7707578a5e2050085c44a6ab59704
Stefan Lekrem <lekrem@icloud.com>
2023-12-20 16:17:09 +0100

    Update ILnMarketsService.cs

--- src/LNMCockpitBackend/Services/ILnMarketsService.cs
+++ src/LNMCockpitBackend/Services/ILnMarketsService.cs
@@ -4,13 +4,13 @@
 
     public interface ILnMarketsService
     {
-        Task<IEnumerable<FuturesPriceHistoryModel>> FuturesGetPriceHistoryAsync(long from, long to, int limit = int.MaxValue);
+        Task<IEnumerable<FuturesPriceHistoryModel>> FuturesGetPriceHistoryAsync(long from, long to, int limit = 1000);
 
-        Task<IEnumerable<FuturesTradeModel>> FuturesGetOpenTradesAsync(long from, long to, int limit = int.MaxValue);
+        Task<IEnumerable<FuturesTradeModel>> FuturesGetOpenTradesAsync(long from, long to, int limit = 1000);
 
-        Task<IEnumerable<FuturesTradeModel>> FuturesGetClosedTradesAsync(long from, long to, int limit = int.MaxValue);
+        Task<IEnumerable<FuturesTradeModel>> FuturesGetClosedTradesAsync(long from, long to, int limit = 1000);
 
-        Task<IEnumerable<FuturesTradeModel>> FuturesGetRunningTradesAsync(long from, long to, int limit = int.MaxValue);
+        Task<IEnumerable<FuturesTradeModel>> FuturesGetRunningTradesAsync(long from, long to, int limit = 1000);
 
         Task<bool> FuturesNewTrade(string side, string type, int leverage);
 
