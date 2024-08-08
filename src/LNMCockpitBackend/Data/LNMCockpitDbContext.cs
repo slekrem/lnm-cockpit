42dbf4673e4c63c96aef5e8fe33db977181d7708
Stefan Lekrem <lekrem@icloud.com>
2024-08-08 11:33:14 +0200

    Create LNMCockpitDbContext.cs

---
+++ src/LNMCockpitBackend/Data/LNMCockpitDbContext.cs
@@ -0,0 +1,12 @@
+﻿namespace LNMCockpit.Data
+{
+    using LNMCockpit.Data.Models;
+    using Microsoft.EntityFrameworkCore;
+
+    public class LNMCockpitDbContext : DbContext
+    {
+        public DbSet<LNMPrice> Models { get; set; }
+
+        public LNMCockpitDbContext(DbContextOptions<LNMCockpitDbContext> options) : base(options) { }
+    }
+}
